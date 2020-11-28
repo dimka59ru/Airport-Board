80de7a2d75f210df0b5a4ed70058fdcb62a38fcd
Дмитрий Суворов <dimka59ru@gmail.com>
2020-11-28 12:49:39 +0500

    Работа с System.Threading.Timer

--- Tests/ABConsole/Program.cs
+++ Tests/ABConsole/Program.cs
@@ -6,6 +6,8 @@ namespace ABConsole
     class Program
     {
         private static Timer timer;
+        private static System.Threading.Timer aTimer;
+
         private static int startSeconds = 190;
         private static int endSeconds = 86400;
 
@@ -16,14 +18,46 @@ namespace ABConsole
             spanSeconds = startSeconds;
 
             timer = new Timer(10000);
-            timer.Elapsed += OnTimedEvent;
-            timer.AutoReset = true;
-            timer.Enabled = true;
-            
+            //timer.Elapsed += OnTimedEvent;
+            //timer.AutoReset = true;
+            //timer.Enabled = true;
+
+
+            var autoEvent = new System.Threading.AutoResetEvent(false);
+            aTimer = new System.Threading.Timer(TimerCallback, autoEvent, 0, 1000);
+            autoEvent.WaitOne();
+            aTimer.Dispose();
+            Console.WriteLine("\nDestroying timer.");
+
             Console.ReadLine();
 
             timer.Stop();
             timer.Dispose();
+        }
+
+        private static void TimerCallback(object state)
+        {
+            System.Threading.AutoResetEvent autoEvent = (System.Threading.AutoResetEvent)state;
+
+            // Каждый вызов таймера прибавляем к прошедшему времени 10 секунд. 
+            spanSeconds += 10;
+
+            if (spanSeconds == 300) // Ускоряем
+            {
+                aTimer.Change(0, 500);
+            }
+
+            if (spanSeconds == 600) // Ускоряем
+            {
+                aTimer.Change(0, 1);
+            }
+
+            if (spanSeconds >= 1000/*endSeconds*/)
+            {
+                autoEvent.Set();
+            }
+
+            DisplayTime();
         }
 
         private static void OnTimedEvent(object sender, ElapsedEventArgs e)
@@ -50,6 +84,11 @@ namespace ABConsole
                 timer.Stop();
             }
 
+            DisplayTime();
+        }
+
+        private static void DisplayTime()
+        {
             TimeSpan t = TimeSpan.FromSeconds(spanSeconds);
 
             string timeNow = string.Format("{0:D2}h:{1:D2}m:{2:D2}s",
